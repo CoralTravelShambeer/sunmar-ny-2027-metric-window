333c18f8b7e1a17871d0f30ab149cd66c7596155
Shamber <147848650+shambeer@users.noreply.github.com>
2026-08-27 12:38:01 +0300

    pop ap ap ap

---
+++ landing.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  schemaVersion: 2,
+  builder: {
+    minVersion: "2.10.0",
+  },
+  project: {
+    name: "sunmar-ny-2027-metric-window",
+  },
+  site: {
+    preset: "sunmar",
+  },
+  stack: {
+    script: "js",
+    markup: "html",
+    styles: "scss",
+  },
+};
